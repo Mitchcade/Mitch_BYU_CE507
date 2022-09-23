6514fbce7efb259d7b6bae31c835e14ba54667f4
Mitchcade <113555791+Mitchcade@users.noreply.github.com>
2022-09-22 22:53:04 -0600

    Add MonomialBasis

--- src/TaylorExpansion.py
+++ src/TaylorExpansion.py
@@ -1,4 +1,7 @@
+# from cmath import pi, sin
 import sympy
+# import numpy as np
+# import matplotlib.pyplot 
 
 def taylorExpansion( fun, a, order ):
     x = list( fun.atoms( sympy.Symbol ) )[0]
@@ -8,3 +11,10 @@ def taylorExpansion( fun, a, order ):
         term = ( df.subs( x, a ) / sympy.factorial( i ) ) * ( x - a )**i
         t += term
     return t
+
+# xx = np.linspace(0,100,100)
+
+# x = np.linspace(0,100,100)
+# a = taylorExpansion(sin(pi*x),1,1)
+
+# matplotlib.pyplot(xx,a)
